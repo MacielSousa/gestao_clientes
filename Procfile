2f5778e6bff18f31352888987b803198e79f6de2
MacielSousa <you@example.com>
2018-07-12 15:51:24 -0300

    Configurando o Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gestao_clientes.wsgi --log-file -
+web: gunicorn website.wsgi --log-file -
--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes321.herokuapp.com']
+ALLOWED_HOSTS = ['open-clientes.herokuapp.com']
 
 
 # Application definition
